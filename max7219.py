be11163c43469d4201e8e956a3b90594fcd9a9f5
seelpro <53879791+seelpro@users.noreply.github.com>
2022-10-16 16:26:37 +0300

    Update max7219.py

--- max7219.py
+++ max7219.py
@@ -97,3 +97,80 @@ class Matrix8x8:
             for m in range(self.num):
                 self.spi.write(bytearray([_DIGIT0 + y, self.buffer[(y * self.num) + m]]))
             self.cs(1)
+
+class maxMatrix:
+    def __init__(self, spi, cs, num, lines):
+        """
+        Driver for cascading matrix (num x lines) of MAX7219 8x8 LED matrices.
+        MultiLine Version (tested on 32 x 32 dots matrix = 4x4 max7219 used)
+        each <line> input connected to output of previous <line>
+        Adapted to Arduino version of library
+        
+        >>> import max7219
+        >>> from machine import Pin, SPI
+        >>> spi = SPI(1)
+        >>> display = max7219.maxMatrix(spi, Pin('X5'), 4, 4)
+        >>> display.text('1234',0, 0,1)
+        >>> display.text('5678',0, 8,1)
+        >>> display.text('90AB',0,16,1)
+        >>> display.text('CDEF',0,24,1)
+        >>> display.show()
+        """
+        self.spi = spi
+        self.cs = cs
+        self.cs.init(cs.OUT, True)
+        self.buffer = bytearray(8 * num * lines)
+        self._command_ba = bytearray(2)
+        self.num = num
+        self.lines = lines
+        fb = framebuf.FrameBuffer(self.buffer, 8 * num, 8 * lines, framebuf.MONO_HMSB)
+        self.framebuf = fb
+        # Provide methods for accessing FrameBuffer graphics primitives. This is a workround
+        # because inheritance from a native class is currently unsupported.
+        # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+        self.fill = fb.fill  # (col)
+        self.pixel = fb.pixel # (x, y[, c])
+        self.hline = fb.hline  # (x, y, w, col)
+        self.vline = fb.vline  # (x, y, h, col)
+        self.line = fb.line  # (x1, y1, x2, y2, col)
+        self.rect = fb.rect  # (x, y, w, h, col)
+        self.fill_rect = fb.fill_rect  # (x, y, w, h, col)
+        self.text = fb.text  # (string, x, y, col=1)
+        self.scroll = fb.scroll  # (dx, dy)
+        self.blit = fb.blit  # (fbuf, x, y[, key])
+        self.init()
+
+    def _write(self, command, data):
+        self._command_ba[0] = command
+        self._command_ba[1] = data
+        self.cs(0)
+        for m in range(self.num * self.lines):
+            self.spi.write(self._command_ba)
+        self.cs(1)
+
+    def init(self):
+        for command, data in (
+            (_SHUTDOWN, 0),
+            (_DISPLAYTEST, 0),
+            (_SCANLIMIT, 7),
+            (_DECODEMODE, 0),
+            (_SHUTDOWN, 1),
+        ):
+            self._write(command, data)
+
+
+    def brightness(self, value):
+        if not 0 <= value <= 15:
+            raise ValueError("Brightness out of range")
+        self._write(_INTENSITY, value)
+
+    def show(self):
+        for y in range(8):
+            self.cs(0)
+            self._command_ba[0] = _DIGIT7 - y 
+            for l in reversed(range(self.lines)):
+                for m in reversed(range(self.num)):
+                  self._command_ba[1] = self.buffer[(y * self.num) + m + l * self.num*8]
+                  self.spi.write(self._command_ba)
+            self.cs(1)
+
